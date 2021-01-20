15e3b1928eb099f1825d20c689eddcfdae42c568
Doug <douglawrence0010@gmail.com>
2021-01-20 12:10:44 +0000

    Incremented version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='zegami-sdk',
-    version='0.1.0',
+    version='0.1.1',
     description='A suite of tools for interacting with Zegami through Python.',
     long_description=README,
     long_description_content_type='text/markdown',
