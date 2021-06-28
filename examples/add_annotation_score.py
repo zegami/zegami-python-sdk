d5ef4c461028c30a5cf44c0f4402e81f2c1cedf2
TracyZhou <tracivar@outlook.com>
2021-06-28 15:59:44 +0100

    Add image_info, mapping nodes and include in output dataset

--- examples/add_annotation_score.py
+++ examples/add_annotation_score.py
@@ -1,10 +1,10 @@
 from zegami_sdk import nodes
 from zegami_sdk.client import ZegamiClient
 
-WORKSPACE_ID = ''
-COLLECTION_ID = ''
+WORKSPACE_ID = 'idUpFsFf'
+COLLECTION_ID = '60d49817ddb415563a6526e1'
 
-zc = ZegamiClient()
+zc = ZegamiClient("mingya.zhou@zegami.com", "GHdUh5deDHXez2G")
 
 workspace = zc.get_workspace_by_id(WORKSPACE_ID)
 collection = workspace.get_collection_by_id(COLLECTION_ID)
@@ -24,3 +24,40 @@ resp = nodes.add_node(
     name="annotation score ims"
 )
 annotation_score_node = resp.get('imageset')
+
+# create image_info dataset to render annotation score as a tsv file
+resp = nodes.add_node(
+    zc,
+    workspace,
+    'image_info',
+    {},
+    'dataset',
+    imageset_parents=annotation_score_node.get('id'),
+    name="annotation score info ds"
+)
+info_node = resp.get('dataset')
+
+# create mapping dataset to map
+join_dataset_id = collection._data.get('imageset_dataset_join_id')
+resp = nodes.add_node(
+    zc,
+    workspace,
+    'mapping',
+    {},
+    'dataset',
+    dataset_parents=[
+        info_node.get('id'),  # first parent is the data node
+        join_dataset_id,  # second parent is the mapping node
+    ],
+    name="annotation score mapping ds"
+)
+mapping_node = resp.get('dataset')
+
+# insert into output parents
+output_dataset_id = collection._data.get('output_dataset_id')
+nodes.add_parent(
+    zc,
+    workspace,
+    output_dataset_id,
+    mapping_node.get('id')
+)
