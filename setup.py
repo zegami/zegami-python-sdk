2e14537c8b2e9d311826b619f88b0b2e885a3767
Andy Haith <andrew.haith@zegami.com>
2021-12-02 10:58:48 +0000

    Blobstore should retry uploads to http blobstore (#71)
    * Blobstore should retry uploads to http blobstore
    
    OnPrem storage urls are http
    
    * version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import pathlib
 from setuptools import setup
 
 # Update version here when you want to increment the version in PyPi
-sdk_version = '0.4.2'
+sdk_version = '0.4.3'
 
 # If no ZEGAMI_SDK_VERSION set use the version
 try:
