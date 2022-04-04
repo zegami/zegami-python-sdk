0404ec6aa60e30fe58a9c1f05bc5daa345503a59
Sofia Beco <sofia.beco@zegami.com>
2022-04-04 13:33:16 +0100

    Small readability changes

--- examples/replace_datafile.py
+++ examples/replace_datafile.py
@@ -2,16 +2,24 @@
 # Copyright 2022 Zegami Ltd
 
 """ Replace datafile of collection."""
-
 from zegami_sdk.client import ZegamiClient
+
+# Input Variables
 
 WORKSPACE_ID = ''
 COLLECTION_ID = ''
+datafile = r"path/to/file"
+is_production = True # Set as False if the collection is on staging
 
-zc = ZegamiClient()
+# Replace data
+
+if is_production:
+    HOME = "https://zegami.com"
+else:
+    HOME = "https://staging.zegami.com"
+
+zc = ZegamiClient(home=HOME)
 workspace = zc.get_workspace_by_id(WORKSPACE_ID)
 collection = workspace.get_collection_by_id(COLLECTION_ID)
-
-datafile = r"path/to/file"
 
 collection.replace_data(datafile)
