4e18f1493ca2b75fd508787fa3d84c5ec460f8b0
Doug <douglawrence0010@gmail.com>
2022-07-13 02:00:02 +0100

    Trained Model Additions

--- zegami_sdk/trained_models/__init__.py
+++ zegami_sdk/trained_models/__init__.py
@@ -5,7 +5,7 @@ import os
 SUPPORTED_MODEL_TYPES = [
     'mrcnn'
 ]
-    
+
 
 class TrainedModel():
     """
@@ -14,27 +14,27 @@ class TrainedModel():
     a model training environment/experiment where parameters for training
     need to be stored.
     """
-    
+
     TYPE = None
-    
+
     def __init__(self, save_path, **kwargs):
         """
         - save_path:
             Path to the trained weights or fully saved model.
-            
+
         - configuration:
             A type-specific configuration required by most model
             implementations.
         """
-        
+
         if self.TYPE not in SUPPORTED_MODEL_TYPES:
             raise ValueError(
                 'TrainedModel type "{}" not supported'.format(self.TYPE))
-            
+
         save_path = save_path.replace('\\', '/').strip('/')
         if not os.path.exists(save_path):
             raise FileNotFoundError(
                 'Could not find model save_path: "{}"'.format(save_path))
-        
+
         self.save_path = save_path
         self.kwargs = kwargs
