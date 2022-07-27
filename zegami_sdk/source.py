c504b72fe63c3a607e5c85384ce030bc3a50245c
Andrew Haith <andrew.haith@zegami.com>
2022-07-27 10:54:57 +0100

    Adjustments to additional_mimes
    Work with a member attribute rather than the class one.
    Default to empty dict rather than None.
    
    Bump version
    
    Revert

--- zegami_sdk/source.py
+++ zegami_sdk/source.py
@@ -131,7 +131,7 @@ class UploadableSource():
     )
 
     def __init__(self, name, image_dir, column_filename='__auto_join__', recursive_search=True, filename_filter=[],
-                 additional_mimes=None):
+                 additional_mimes={}):
         """
         Used in conjunction with create_collection().
 
@@ -142,6 +142,10 @@ class UploadableSource():
         To limit to an allowed specific list of filenames, provide
         'filename_filter'. This filter will check against
         os.path.basename(filepath).
+
+        Common mime types are inferred from the file extension,
+        but a dict of additional mime type mappings can be provided eg to
+        cater for files with no extension.
         """
 
         self.name = name
@@ -152,7 +156,7 @@ class UploadableSource():
         self._source = None
         self._index = None
 
-        UploadableSource.IMAGE_MIMES = {**UploadableSource.IMAGE_MIMES, **additional_mimes}
+        self.image_mimes = {**UploadableSource.IMAGE_MIMES, **additional_mimes}
 
         # Check the directory exists
         if not os.path.exists(image_dir):
@@ -390,15 +394,14 @@ class UploadableSource():
 
         return uploadable_sources
 
-    @classmethod
-    def _get_mime_type(cls, path) -> str:
+    def _get_mime_type(self, path) -> str:
         """Gets the mime_type of the path. Raises an error if not a valid image mime_type."""
         if '.' not in path:
-            return cls.IMAGE_MIMES['']
+            return self.image_mimes['']
         ext = os.path.splitext(path)[-1]
-        if ext in cls.IMAGE_MIMES.keys():
-            return cls.IMAGE_MIMES[ext]
-        raise TypeError('"{}" is not a supported image mime_type ({})'.format(path, cls.IMAGE_MIMES))
+        if ext in self.image_mimes.keys():
+            return self.image_mimes[ext]
+        raise TypeError('"{}" is not a supported image mime_type ({})'.format(path, self.image_mimes))
 
 
 class UrlSource(UploadableSource):
